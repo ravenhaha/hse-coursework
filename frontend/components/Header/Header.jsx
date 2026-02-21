a4390546c7a6de5c7363bf25d0e967bdb066931b
Kiril Bazhenov <kirillbazhenov22@gmail.com>
2026-02-21 13:06:08 +0300

    init: front and back

---
+++ frontend/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+function Header() {
+    return (
+        <div className={styles.Header}>
+            The header
+        </div>
+    )
+}
+
+export default Header;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header/Header.jsx";
+
+function App() {
+
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default App
