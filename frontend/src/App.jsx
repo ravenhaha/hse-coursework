9eb34482f86d446686509a8984705185b06cb9e1
Kiril Bazhenov <kirillbazhenov22@gmail.com>
2026-03-05 13:31:40 +0300

    add new file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import Header from "../components/Header/Header.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout/MainLayout'
+import HomePage from './pages/HomePage/HomePage.jsx'
+import WorkspaceLayout from "./layouts/WorkspaceLayout/WorkspaceLayout.jsx";
+import WorkspacePage from "./pages/WorkspacePage/WorkspacePage.jsx";
+import DevNav from "./components/DevNav/DevNav.jsx";
 
-function App() {
+export default function App() {
+    return (
+        <BrowserRouter>
+            <DevNav />
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<HomePage />} />
+                </Route>
 
-  return (
-    <div>
-        <Header />
-    </div>
-  )
+                <Route element={<WorkspaceLayout />}>
+                    <Route path="/workspace" element={<WorkspacePage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
-export default App
---
+++ frontend/src/components/DevNav/DevNav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function DevNav() {
+    return (
+        <div style={{ padding: 20, display: "flex", gap: 10 }}>
+            <Link to="/">Home</Link>
+            <Link to="/workspace">About</Link>
+        </div>
+    );
+}
+
+export default DevNav;
